27aaa311077639e6019eaeb395ab94af309ed73c
Abhishek Bose <boseabhishek2@gmail.com>
2024-11-14 15:21:59 +0530

    [Frontend] Refactoring in css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,18 @@ import "fomantic-ui-css/semantic.css"
 import "./css/styles.css"
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './components/screens/home/Home';
-import { useWindowSize } from './hooks/useWindowSize';
 import { Game } from "./components/screens/game/Game";
 import { useWebSocket } from "./hooks/useWebSocket";
 import { Lobby } from "./components/screens/lobby/Lobby";
 
 
 function App() {
-    const windowSize = useWindowSize()
-    const socket     = useWebSocket()
+    const socket = useWebSocket()
     return (
-        <div style={{
-            width: windowSize.width,
-            height: windowSize.height,
-            backgroundColor: "#0C0D24",
-            color: "white",
-        }}> 
+        <div className="app">
             {socket && <Router>
                 <Routes>
-                    <Route path="/" element={<Home socket={socket} windowSize={windowSize}/>}></Route>
+                    <Route path="/" element={<Home socket={socket}/>}></Route>
                     <Route path="/lobby" element={<Lobby socket={socket}/>}></Route>
                     <Route path="/game/:gameId" element={<Game socket={socket}/>}></Route>
                 </Routes>
